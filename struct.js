f12a345d1a79d829eb751c1acc1f9a09e1992278
angribird0js <jaehsh@naver.com>
2022-12-15 15:18:30 +0900

    자료형 js 추가

---
+++ struct.js
@@ -0,0 +1,18 @@
+
+class Article {
+
+    constructor( dnum, ttl, txt, day) {
+    this.dnum = dnum //글번호
+    this.ttl = ttl // 제독
+    this.txt = txt // 본문
+    this.day = day // 날짜
+    }
+}
+
+// new Article 선언해서 사용해야 하며
+// 목록에 넣는 방법은 교재 397p 참고
+// 별도의 배열에 푸시해서 사용하면 됩니다.
+// storage 에도 들거가야 하며 콘솔로그로 들어가는 값 확인필요.
+
+
+
