b10d34b8214c8c77bbe2cf0299d2ff0105e3f807
SooBookE <thksl229@gmail.com>
2022-12-15 17:12:24 +0900

    업로딩

--- main.js
+++ main.js
@@ -2,18 +2,28 @@ document.addEventListener("DOMContentLoaded",()=>{
     
     function refresh(){
         const list_container = document.createElement('div');
-        const list_num = document.createElement("div");
-        const list_title = document.createElement("div");
-        const list_writer = document.createElement("div");
-        const list_view = document.createElement("div");
-        const list_date = document.createElement("div");
+        const list_dnum = document.createElement("div");
+        const list_ttl = document.createElement("div");
+        const list_author = document.createElement("div");
+        const list_vcnt = document.createElement("div");
+        const list_day = document.createElement("div");
 
         const container = document.getElementsByClassName('container');
         
-        container[0].appendChild(list_container).append(list_num, list_title, list_writer, list_view, list_date);
-        // list_container에 display: flex 넣어야 합니다!
+        container[0].appendChild(list_container).append(list_dnum, list_ttl, list_author, list_vcnt, list_day);
+        list_container.setAttribute("class",'titles');
+        list_dnum.setAttribute("class",'dnum');
+        list_ttl.setAttribute("class",'ttl');
+        list_author.setAttribute("class",'author');
+        list_vcnt.setAttribute("class",'vcnt');
+        list_day.setAttribute("class",'day');
     }
+
+
+    const ranArr = [{},{},{}]
+
+    localStorage.setItem('value',)
     
-    
+
 
 })
