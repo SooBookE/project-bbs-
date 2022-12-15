6778e7c843a955036f7ed093c33b1ea84f36e120
SooBookE <thksl229@gmail.com>
2022-12-15 15:20:01 +0900

    업로딩

---
+++ main.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    
+    function refresh(){
+        const list_container = document.createElement('div');
+        const list_num = document.createElement("div");
+        const list_title = document.createElement("div");
+        const list_writer = document.createElement("div");
+        const list_view = document.createElement("div");
+        const list_date = document.createElement("div");
+
+        const container = document.getElementsByClassName('container');
+        
+        container.appendChild(list_container).apeend(list_num, list_title, list_writer, list_view, list_date);
+        // list_container에 display: flex 넣어야 합니다!
+    }
+    const insert = document.querySelector("button");
+
+    insert.addEventListener("click",refresh());
+
+
+})
