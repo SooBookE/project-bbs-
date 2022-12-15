2afa97e10cecb8e2b59238b2b36eabeeec2a8070
SooBookE <thksl229@gmail.com>
2022-12-15 16:21:46 +0900

    업로딩

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 document.addEventListener("DOMContentLoaded",()=>{
     
     function refresh(){
-        const list_container = document.createElement('tr');
-        const list_num = document.createElement("td");
-        const list_title = document.createElement("td");
-        const list_writer = document.createElement("td");
-        const list_view = document.createElement("td");
-        const list_date = document.createElement("td");
+        const list_container = document.createElement('div');
+        const list_num = document.createElement("div");
+        const list_title = document.createElement("div");
+        const list_writer = document.createElement("div");
+        const list_view = document.createElement("div");
+        const list_date = document.createElement("div");
 
-        const table = document.querySelector('table');
+        const container = document.getElementsByClassName('container');
         
-        table.appendChild(list_container).append(list_num, list_title, list_writer, list_view, list_date);
+        container[0].appendChild(list_container).append(list_num, list_title, list_writer, list_view, list_date);
         // list_container에 display: flex 넣어야 합니다!
     }
     const insert = document.querySelector("button");
@@ -19,24 +19,3 @@ document.addEventListener("DOMContentLoaded",()=>{
 
 
 })
-// document.addEventListener("DOMContentLoaded",()=>{
-    
-//     function refresh(){
-//         const list_container = document.createElement('div');
-//         const list_num = document.createElement("div");
-//         const list_title = document.createElement("div");
-//         const list_writer = document.createElement("div");
-//         const list_view = document.createElement("div");
-//         const list_date = document.createElement("div");
-
-//         const container = document.getElementsByClassName('container');
-        
-//         container[0].appendChild(list_container).append(list_num, list_title, list_writer, list_view, list_date);
-//         // list_container에 display: flex 넣어야 합니다!
-//     }
-//     const insert = document.querySelector("button");
-
-//     insert.addEventListener("click",refresh);
-
-
-// })
